780220ae6b48a5d20eb8b81b5ab1da24524a8114
Tim Harshman <goteamtim+git@gmail.com>
2017-04-04 07:28:51 -0700

    style and scripts

---
+++ drumkit/js/main.js
@@ -0,0 +1,6 @@
+(function(){
+    var keys = document.querySelectorAll('drum-key');
+    //Collect the keys
+    //add an event listener to the divs
+    //create function that adds the playing class then plays the sound and removes the class when finished
+})();
