a8f09bc6268e3ce70eceb2c3ca8f69be25ece34e
Tim Harshman <goteamtim+git@gmail.com>
2017-04-04 08:03:49 -0700

    More style and arrayify the nodes

--- drumkit/js/main.js
+++ drumkit/js/main.js
@@ -1,7 +1,10 @@
 (function(){
-    var keys = document.querySelectorAll('drum-key');
+    var keys = Array.from(document.querySelectorAll('.drum-key'));
+    //var keysArray = [].slice.call(keys);
     //Collect the keys
     //add an event listener to the divs
     //create function that adds the playing class then plays the sound and removes the class when finished
-    
+    function addListener(element){
+        
+    }
 })();
