a20214ea8cade27115cd9ef6973587c8287245b3
Tim Harshman <goteamtim+git@gmail.com>
2017-04-04 07:39:37 -0700

    Add more color

--- drumkit/js/main.js
+++ drumkit/js/main.js
@@ -3,4 +3,5 @@
     //Collect the keys
     //add an event listener to the divs
     //create function that adds the playing class then plays the sound and removes the class when finished
+    
 })();
