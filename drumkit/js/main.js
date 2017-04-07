c31f2b05e780b0e59f566e8140a1ce0c2c2ad341
Tim Harshman <goteamtim+git@gmail.com>
2017-04-06 22:11:30 -0700

    drumkit add sounds and some functions

--- drumkit/js/main.js
+++ drumkit/js/main.js
@@ -1,10 +1,19 @@
 (function(){
     var keys = Array.from(document.querySelectorAll('.drum-key'));
-    //var keysArray = [].slice.call(keys);
+    var keysArray = [].slice.call(keys);
     //Collect the keys
     //add an event listener to the divs
     //create function that adds the playing class then plays the sound and removes the class when finished
+    keysArray.forEach(addListener);
     function addListener(element){
-        
+        element.addEventListener('click',function(){
+            playSound();
+        });
+    };
+
+    function playSound(){
+        var audio = new Audio('./sounds/snare.wav');
+        audio.play();
     }
+
 })();
