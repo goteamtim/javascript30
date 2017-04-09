b93505d37deaf85f5fd0f7820832c87e053dacb2
Tim Harshman <goteamtim+git@gmail.com>
2017-04-09 12:51:45 -0700

    Add sound object

--- drumkit/js/main.js
+++ drumkit/js/main.js
@@ -1,6 +1,17 @@
 (function(){
     var keys = Array.from(document.querySelectorAll('.drum-key'));
     var keysArray = [].slice.call(keys);
+    var soundFiles = {
+        "A": 'clap.wav',
+        "S": 'hihat.wav',
+        "D": 'kick.wav',
+        "F": 'openhat.wav',
+        "G": 'boom.wav',
+        "H": 'ride.wav',
+        "J": 'snare.wav',
+        "K": 'tom.wav',
+        "L": 'tink.wav',
+    };
     
     //add an event listener to the divs
     //create function that adds the playing class then plays the sound and removes the class when finished
