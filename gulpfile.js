3abd30ab88e39376c90fadf95e7ae2d64e4c0f54
Tim Harshman <goteamtim+git@gmail.com>
2017-04-13 21:12:38 -0700

    GOT BROWSERSYNC WORKING

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,27 @@ var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
 // Static server
-gulp.task('test', function() {
+gulp.task('browsersync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
     
-    gulp.watch("/drumkit/*.*").on('change',reload);
+    //gulp.watch("*.*").on('change',function(){console.log("gulp watched")});  //reload
+});
+
+// Default task
+gulp.task('default',['browsersync','watch']);
+// gulp.task('default', function() {
+//     gulp.start('browsersync');
+//     gulp.start('watch');
+// });
+
+// Watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('./*.*', reload);
+
 });
